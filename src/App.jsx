89e072c26e4d86c876eb41f1313712eb3f9421fd
hollylolly117-spec <hollylolly117@gmail.com>
2026-05-27 01:31:56 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 20, fontFamily: "system-ui" }}>
+      <h1>Static is running 🚀</h1>
+      <p>Your Vite + React app is working.</p>
+    </div>
+  )
+}
